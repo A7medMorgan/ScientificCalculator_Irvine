fae20e798e350d6debc3c31cf44b5c2b56f6bbaf
zeadmmdouh <59164364+zeadmmdouh@users.noreply.github.com>
2019-12-27 19:04:36 +0200

    Update main.asm

--- Irvine/Project_Template/main.asm
+++ Irvine/Project_Template/main.asm
@@ -19,9 +19,41 @@ num1			real4 ?
 num2			real4 ?
 total           dword ?
 choice_number	dword ?
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;ZEAD;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+prompt_1			BYTE	"First Number: ", 0
+prompt_2			BYTE	"Second Number: ", 0
+firstNumber			DWORD	?							 ; integer entered by user
+secondNumber		DWORD	?							 ; second integer entered by user.
+goodBye				BYTE	"Goodbye",0
+
+remainder			DWORD	?
+remainderString		BYTE	" remainder ",0
+
+; Extra Credit
+
+EC1warn				BYTE	"The second number must be less than the first!", 0
+ 
+EC2string			BYTE	"EC: Floating-point value: ", 0
+EC2FloatingPoint	REAL4	?	; short real single precision floating point variable
+oneThousand			DWORD	1000						; to convert an int to a floating point number rounded to .001 (can be changed to increase or decrease precision)
+bigInt			    DWORD	0							; represents the floating point number multiplied by 1000
+ECremainder			DWORD	?							; for floating point creation
+dot					BYTE	".",0						; to serve as the decimal place of a floating point number
+firstPart			DWORD	?							; for the first part of the floating point representation of the quotient
+secondPart			DWORD	?							; fot the part of the floating point number after the decimal place
+temp				DWORD	?							; temporary holder for floating point creation
+EC3prompt			BYTE	"EC: Would you like to play again? Enter 1 for YES or 0 for NO: ", 0
+EC3explain			BYTE	"**EC: This program loops until the user decides to quit.", 0
+EC3response			DWORD	?							; BOOL for user to loop or exit.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .code
 main PROC
+;//////////////////////////ZEAD/////////////////////////////;
+
+call divide
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	; Output the title
 		mov		edx, OFFSET codeTitle
 		call	WriteString
@@ -119,5 +151,109 @@ finit
 	    call crlf
 		
 		perc endp
+		divide PROC
+
+	
+	
+		call	CrLf
+		mov		edx, OFFSET EC3explain
+		call	WriteString
+		call	CrLf
+
+	
+
+			; get firstNumber
+top:
+			mov		edx, OFFSET prompt_1
+			call	WriteString
+			call	ReadInt
+			mov		firstNumber, eax
+
+
+			; get secondNumber
+			mov		edx, OFFSET prompt_2
+			call	WriteString
+			call	ReadInt
+			mov		secondNumber, eax
+
+			; **EC: Jump if second number greater than first
+			mov		eax, secondNumber
+			cmp		eax, firstNumber
+			jg		Warning
+			jle		Calculate
+
+Warning:
+			mov		edx, OFFSET EC1warn
+			call	WriteString
+			call	CrLf
+			jg		JumpToLoop				; jump if secondNumber > firstNumber
+
+
+Calculate:		; Calculate Required Values
+			
+
+			
+
+				; EC floating point representation of quotient and remainder
+				fld		firstNumber					; load firstNumber (integer) into ST(0)
+				fdiv	secondNumber				; divide firstNumber by secondNumber ?
+				fimul	oneThousand
+				frndint	
+				fist	bigInt
+				fst		EC2FloatingPoint			; take value off stack, put it in EC2FloatingPoint
+
+			; Display Results
+
+			
+
+				; EC2 Output
+				mov		edx, OFFSET EC2string
+				call	WriteString
+				mov		edx, 0
+				mov		eax, bigInt
+				cdq
+				mov		ebx, 1000
+				cdq
+				div		ebx
+				mov		firstPart, eax
+				mov		ECremainder, edx
+				mov		eax, firstPart
+				call	WriteDec
+				mov		edx, OFFSET dot
+				call	WriteString
+
+				;calculate remainder
+				mov		eax, firstPart
+				mul		oneThousand
+				mov		temp, eax
+				mov		eax, bigInt
+				sub		eax, temp
+				mov		secondPart, eax
+				call	WriteDec
+				call	CrLf
+
+		; Loop until user quits
+		; prompts the user to enter a 0 or 1 to continue looping.
+		; if they do want to play again, it takes them to section 'top'
+		; skipping the instrucitons
+
+				; get response for loop
+
+JumpToLoop:			mov		edx, OFFSET EC3prompt
+					call	WriteString
+					call	ReadInt
+					mov		EC3response, eax
+					cmp		eax, 1
+					je		top				; jump to top if response == 1
+
+
+				; Say Goodbye
+					mov		edx, OFFSET goodBye
+					call	WriteString
+					call	CrLf
+
+	ret	; exit to operating system
+divide ENDP
+
 
 END main
